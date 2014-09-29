b1fa4518c8012b349c006778b64b419809804da9
Brent Thorn <b6sw@yahoo.com>
2014-09-28 22:38:19 -0400

    test-all no longer creates a temp file; updated some input files

--- test/test-all.bat
+++ test/test-all.bat
@@ -6,16 +6,17 @@ If Exist test.log Del test.log
 
 For /R %%f In (*.in.txt) Do Call :Test "%%f"
 
-Del test.tmp
 Echo %fails% failed. >> test.log
 GoTo :EOF
 
 :Test
 Set f=%~1
-..\VF -v0 %1 > test.tmp
-FC test.tmp "%f:~0,-6%v0.txt" > NUL
-If ErrorLevel 1 (
-  Echo %f% failed. >> test.log
-  Set /A fails+=1
+For /F "usebackq tokens=* delims=" %%a In (`..\VF -v0 %1`) Do (
+  Echo %%a|FindStr /G:/ "%f:~0,-6%v0.txt" > NUL
+  If ErrorLevel 1 (
+    Echo %f% failed with "%%a". >> test.log
+    Set /A fails+=1
+    GoTo :EOF
+  )
 )
 GoTo :EOF
